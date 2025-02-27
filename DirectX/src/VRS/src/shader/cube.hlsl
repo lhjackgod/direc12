4be721644c4fcdcf9ee6c33e11572ac79b1c8c49
lhjack <lhjackgod@163.com>
2025-02-27 15:18:25 +0800

    add the lerp for hlsl

--- DirectX/src/VRS/src/shader/cube.hlsl
+++ DirectX/src/VRS/src/shader/cube.hlsl
@@ -27,6 +27,10 @@ PSInput VSMain(VSInput input)
 
 float4 PSMain(PSInput input) : SV_TARGET
 {
+    const float pi = 3.1415926;
     float4 color = input.color;
-    return color;
+    float s = 0.5f * sin(2 * time.x - 0.25f * pi) + 0.5f;
+    float4 otherColor = float4(1.0, 1.0, 0.0, 1.0);
+    float4 c = lerp(otherColor, color, s);
+    return c;
 }
